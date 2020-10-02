472b639113473b2a38e622e98509156748758d1f
Nicolas Decat <nicolas.decat@gmail.com>
2020-10-02 14:27:59 +0200

    Exercice 5 - done and test passes

---
+++ ExA5/src/test/java/com/bnpparibasfortis/TestPerson.java
@@ -0,0 +1,41 @@
+package com.bnpparibasfortis;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class TestPerson {
+
+    private Person person = new Person(1, "John", "Doe", LocalDate.of(1996, 5, 05));
+
+    @Test
+    public void calculateAge_personShouldBe24() {
+
+        // arrange
+        Integer expected = 24; // 24 years old
+
+        // act
+        Integer result = person.calculateAge();
+
+        // assert
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void toStringSentenceStartsWithPerson() {
+
+        //arrange
+        String expected = "Person";
+
+        //act
+        String[] arrayPersonIntoString = (person.toString()).split(" ");
+        String firstWord = arrayPersonIntoString[0];
+
+        //assert
+        assertThat(expected, is(firstWord));
+    }
+}
